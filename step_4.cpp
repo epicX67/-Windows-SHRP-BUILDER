9dbd4cd38ae90da788850440f72a0dd3d0708482
epicX67 <46811948+epicX67@users.noreply.github.com>
2019-09-09 10:11:45 +0530

    Create step_4.cpp

---
+++ step_4.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+
+
+int main(){
+	int device_type,partition_size;
+	printf("                                   _ _ _ _\n");
+	printf("   __ __  __  _ __ _   _ _  _     /_ _   /\n");
+	printf("  / _ _/ / / / / __ / / __  /    _ _ _/ / \n");
+	printf(" /__    / /_/ / /_/  / /_/ /   /  _ _ _/  \n");
+	printf(" ___/  / __  / _,   / ____/   / /_ _      \n");
+	printf("/__ _ /_/ /_/_/ |_ /_/       /__ __/      \n");
+	printf("                                                  \n");
+	printf("                                                  \n");
+	printf("Last step(Press any key)-                    \n");
+	printf("--------------------------------------------------\n");
+	getch();
+	system("laster.cmd");
+
+}
